10090be9d17f76e598898e3cadc00a365399e3b2
Ananya2410 <ananyasengupta2410@gmail.com>
2024-03-08 23:12:13 +0530

    Lecture 2

---
+++ arithop.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num1,num2;
+    cout<<"Enter 1st number: "<<endl;
+    cin>>num1;
+    cout<<"Enter 2nd number: "<<endl;
+    cin>>num2;
+
+    cout<<"Addition: "<<num1+num2<<endl;
+    cout<<"Substraction: "<<num1-num2<<endl;
+    cout<<"Multiplication: "<<num1*num2<<endl;
+    cout<<"Division: "<<num1/num2<<endl;
+    cout<<"Modulus: "<<num1%num2<<endl;
+
+    return 0;
+}
