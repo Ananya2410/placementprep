bd31c157c739cc40bca5e1c61f0d422d4d1ad3ed
Ananya2410 <ananyasengupta2410@gmail.com>
2024-03-08 23:45:02 +0530

    Lecture 2

---
+++ relationalop.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num1,num2;
+    cout<<"Enter 1st number: "<<endl;
+    cin>>num1;
+    cout<<"Enter 2nd number: "<<endl;
+    cin>>num2;
+    // 0=false , 1=true
+    cout<<(num1==num2)<<endl;
+    cout<<(num1!=num2)<<endl;
+    cout<<(num1>=num2)<<endl;
+    cout<<(num1<=num2)<<endl;
+
+
+    return 0;
+}
