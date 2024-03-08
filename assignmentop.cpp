f928a20239546964e4b108bc86165c6cb17dfca7
Ananya2410 <ananyasengupta2410@gmail.com>
2024-03-09 00:30:49 +0530

    Lecture 2

---
+++ assignmentop.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num1,num2;
+    cout<<"Enter 1st number: "<<endl;
+    cin>>num1;
+    cout<<"Enter 2nd number: "<<endl;
+    cin>>num2;
+    
+    num1+=3;
+    num2-=2;
+    cout<<(num1)<<endl;
+    cout<<(num2)<<endl;
+
+    return 0;
+}
